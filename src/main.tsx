e57a241d7e5b6a3d7a3a1f375ad2d8086f660de8
John M <entity.john@gmail.com>
2024-09-20 12:24:40 -0500

    add mui baseline css and tidy up our app css

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { App } from './app/App'
+import { CssBaseline } from '@mui/material'
 import './app/theme.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <CssBaseline />
     <BrowserRouter>
       <App />
     </BrowserRouter>
