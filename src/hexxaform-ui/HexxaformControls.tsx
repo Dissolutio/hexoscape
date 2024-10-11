e54a7ef05ed99c71b6283cf4485883393682927c
John M <entity.john@gmail.com>
2024-10-10 20:02:27 -0500

    extract component: LoadSaveMapButtons

--- src/hexxaform-ui/HexxaformControls.tsx
+++ src/hexxaform-ui/HexxaformControls.tsx
@@ -1,29 +1,12 @@
 import styled from 'styled-components'
-import Button from '@mui/material/Button'
-
-import { ChangeEvent, useState } from 'react'
-import { giantsTable } from '../game/setup/maps/giantsTable'
-import { forsakenWaters } from '../game/setup/maps/forsakenWaters'
-import { BoardHexes, HexMap } from '../game/types'
-import {
-  hexagonScenario,
-  rectangleScenario,
-} from '../game/hexxaform/hexxaform-setup'
-import { cirdanGardenMap } from '../game/setup/maps/cirdanGarden'
-import { useLocalMapMemory } from './useLocalMapMemory'
-import { translateHexagonBoardHexesToNormal } from '../game/setup/hex-gen'
-import {
-  Box,
-  ButtonGroup,
-  Container,
-  Snackbar,
-  SnackbarCloseReason,
-} from '@mui/material'
-import { useMapContext } from './useMapContext'
+import { ChangeEvent } from 'react'
+import { Box, Button, ButtonGroup } from '@mui/material'
 import { GiArrowCursor } from 'react-icons/gi'
-import SplitButton from './SplitButton'
 import { MdFileUpload } from 'react-icons/md'
+
+import { useMapContext } from './useMapContext'
 import { BgioProps } from '../game/hexxaform/hexxaform-types'
+import { LoadSaveMapButtons } from './LoadSaveMapButtons'
 
 export const HexxaformControls = ({ boardHexes, hexMap, moves }: BgioProps) => {
   const uploadElementID = 'upload'
@@ -151,119 +134,6 @@ const StyledVisuallyHiddenFileInput = styled.input`
   white-space: nowrap;
   width: 1;
 `
-const LoadSaveMapButtons = ({ boardHexes, hexMap, moves }: BgioProps) => {
-  const [snackbarMsg, setSnackbarMsg] = useState('')
-  const isOpen = Boolean(snackbarMsg)
-  const handleClose = (
-    event: React.SyntheticEvent | Event,
-    reason?: SnackbarCloseReason
-  ) => {
-    if (reason === 'clickaway') {
-      return
-    }
-    setSnackbarMsg('')
-  }
-  const currentSaveableMap = { boardHexes, hexMap }
-  const { map1, setMap1, map2, setMap2, map3, setMap3 } = useLocalMapMemory()
-  const isMap1 =
-    Object.keys(map1?.boardHexes ?? {})?.length > 0 && Boolean(map1?.hexMap)
-  const isMap2 =
-    Object.keys(map2?.boardHexes ?? {})?.length > 0 && Boolean(map2?.hexMap)
-  const isMap3 =
-    Object.keys(map3?.boardHexes ?? {})?.length > 0 && Boolean(map3?.hexMap)
-  const handleLoadMap1 = () => {
-    if (isMap1) {
-      moves.loadMap({ boardHexes: map1.boardHexes, hexMap: map1.hexMap })
-      setSnackbarMsg('Map 1 loaded')
-    }
-  }
-  const handleLoadMap2 = () => {
-    if (isMap2) {
-      moves.loadMap({ boardHexes: map2.boardHexes, hexMap: map2.hexMap })
-      setSnackbarMsg('Map 2 loaded')
-    }
-  }
-  const handleLoadMap3 = () => {
-    if (isMap3) {
-      moves.loadMap({ boardHexes: map3.boardHexes, hexMap: map3.hexMap })
-      setSnackbarMsg('Map 3 loaded')
-    }
-  }
-  const handleLoadHexagonMap = () => {
-    moves.loadMap(hexagonScenario)
-    setSnackbarMsg(`Loaded map: ${hexagonScenario.hexMap.mapName}`)
-  }
-  const handleLoadRectangleMap = () => {
-    moves.loadMap(rectangleScenario)
-    setSnackbarMsg(`Loaded map: ${rectangleScenario.hexMap.mapName}`)
-  }
-  const handleLoadGiantsTable = () => {
-    moves.loadMap({
-      boardHexes: giantsTable.boardHexes,
-      hexMap: giantsTable.hexMap,
-    })
-    setSnackbarMsg(`Loaded map: ${giantsTable.hexMap.mapName}`)
-  }
-  const handleLoadForsakenWaters = () => {
-    moves.loadMap({
-      boardHexes: forsakenWaters.boardHexes,
-      hexMap: forsakenWaters.hexMap,
-    })
-    setSnackbarMsg(`Loaded map: ${forsakenWaters.hexMap.mapName}`)
-  }
-  const handleLoadCirdanGarden = () => {
-    const translatedBoardHexes = translateHexagonBoardHexesToNormal(
-      cirdanGardenMap.boardHexes,
-      cirdanGardenMap.hexMap.mapSize
-    )
-    moves.loadMap({
-      boardHexes: translatedBoardHexes,
-      // boardHexes: cirdanGardenMap.boardHexes,
-      hexMap: cirdanGardenMap.hexMap,
-    })
-    setSnackbarMsg(`Loaded map: ${cirdanGardenMap.hexMap.mapName}`)
-  }
-  const handleSaveMap1 = () => {
-    setMap1(currentSaveableMap)
-    setSnackbarMsg(`Saved to local map slot 1`)
-  }
-  const handleSaveMap2 = () => {
-    setMap2(currentSaveableMap)
-    setSnackbarMsg(`Saved to local map slot 2`)
-  }
-  const handleSaveMap3 = () => {
-    setMap3(currentSaveableMap)
-    setSnackbarMsg(`Saved to local map slot 3`)
-  }
-  return (
-    <>
-      <Snackbar
-        open={isOpen}
-        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
-        autoHideDuration={5000}
-        onClose={handleClose}
-        message={snackbarMsg}
-      />
-      <SplitButton
-        isMap1={isMap1}
-        isMap2={isMap2}
-        isMap3={isMap3}
-        handleLoadMap1={handleLoadMap1}
-        handleLoadMap2={handleLoadMap2}
-        handleLoadMap3={handleLoadMap3}
-        handleSaveMap1={handleSaveMap1}
-        handleSaveMap2={handleSaveMap2}
-        handleSaveMap3={handleSaveMap3}
-        handleLoadGiantsTable={handleLoadGiantsTable}
-        handleLoadForsakenWaters={handleLoadForsakenWaters}
-        handleLoadCirdanGarden={handleLoadCirdanGarden}
-        handleLoadHexagonMap={handleLoadHexagonMap}
-        handleLoadRectangleMap={handleLoadRectangleMap}
-      />
-    </>
-  )
-}
-
 export const StyledSection = styled.section`
   display: flex;
   flex-direction: row;
