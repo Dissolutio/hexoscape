b03a843d77539e12138f8d0ffc938ad1352a3dbf
John M <entity.john@gmail.com>
2024-11-10 10:42:37 -0600

    not quite working as simply as expected

--- src/shared/world/InstanceSolidHexCap.tsx
+++ src/shared/world/InstanceSolidHexCap.tsx
@@ -1,5 +1,5 @@
-import { useRef, useLayoutEffect } from 'react'
-import { ThreeEvent } from '@react-three/fiber'
+import { useRef, useLayoutEffect, useEffect, useMemo } from 'react'
+import { ThreeEvent, useFrame } from '@react-three/fiber'
 import {
   BufferGeometry,
   Color,
@@ -26,6 +26,8 @@ type Props = {
   handleHover: (id: string) => void
   handleUnhover: (id: string) => void
 }
+
+const tempColor = new Color()
 const InstanceSolidHexCap = ({
   boardHexes,
   onClick,
@@ -33,6 +35,7 @@ const InstanceSolidHexCap = ({
   handleHover,
   handleUnhover,
 }: Props) => {
+  const prevRef = useRef('')
   const instanceRef = useRef<
     InstancedMesh<
       BufferGeometry<NormalBufferAttributes>,
@@ -43,10 +46,16 @@ const InstanceSolidHexCap = ({
   const solidCapHexesArray = Object.values(boardHexes).filter((bh) => {
     return !isFluidTerrainHex(bh.terrain)
   })
+  const colorArray = useMemo(
+    () => Float32Array.from(new Array(solidCapHexesArray.length).fill(0).flatMap((_, i) => tempColor.set(hexTerrainColor[solidCapHexesArray[i].terrain]).toArray())),
+    []
+  )
   /* countOfCapHexes: Right now, this is simply all solid terrain boardHexes. But with overhangs, and "floaters", this would be calculated. */
   const countOfCapHexes = solidCapHexesArray.length
+
   // effect where we create and update instance mesh for each subterrain mesh
   useLayoutEffect(() => {
+    console.log("🚀 ~ useLayoutEffect ~ useLayoutEffect:")
     const placeholder = new Object3D()
     solidCapHexesArray.forEach((boardHex, i) => {
       const altitude = boardHex.altitude
@@ -58,15 +67,25 @@ const InstanceSolidHexCap = ({
       const { x, z } = getBoardHex3DCoords(boardHex)
       const capPosition = new Vector3(x, yAdjustSolidCap, z)
       const heightScaleSolidCap = halfLevel
+
+
+      // COLOR SET STILL NEEDED WITH NEW useFrame ??
       const terrainColor = new Color(hexTerrainColor[boardHex.terrain])
+      if (solidCapHexesArray[i].id === hoverID) {
+        instanceRef.current.setColorAt(i, new Color("#fff"))
+      } else {
+        // update instance color
+        instanceRef.current.setColorAt(i, terrainColor)
+      }
+
+
+
       // set placeholder position
       placeholder.position.set(capPosition.x, capPosition.y, capPosition.z)
       // set placeholder scale
       placeholder.scale.set(1, heightScaleSolidCap, 1)
       // update placeholder matrix
       placeholder.updateMatrix()
-      // update instance color
-      instanceRef.current.setColorAt(i, terrainColor)
       // update instance matrix
       instanceRef.current.setMatrixAt(i, placeholder.matrix)
     })
@@ -74,6 +93,35 @@ const InstanceSolidHexCap = ({
     instanceRef.current.instanceMatrix.needsUpdate = true
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [boardHexes])
+
+  useEffect(() => { prevRef.current = hoverID }, [hoverID])
+
+  useFrame(() => {
+    for (let i = 0; i < countOfCapHexes; i++) {
+      if (hoverID === prevRef.current) {
+        if (solidCapHexesArray[i].id === hoverID) {
+          console.log("🚀 ~ Hex should be WHITE:", solidCapHexesArray[i].id)
+          // update instance color
+          // instanceRef.current.setColorAt(i, new Color('#fff'))
+          // tempColor.set('#fff').toArray(colorArray, i * 3)
+          tempColor.set(hexTerrainColor[solidCapHexesArray[i].terrain]).toArray(colorArray, ((i + 1) * 3))
+        } else {
+          console.log("🚀 ~ Hex should be terrain:", solidCapHexesArray[i].id)
+          // instanceRef.current.setColorAt(i, new Color(hexTerrainColor[solidCapHexesArray[i].terrain]))
+          tempColor.set(hexTerrainColor[solidCapHexesArray[i].terrain]).toArray(colorArray, i * 3)
+        }
+        instanceRef.current.geometry.attributes.color.needsUpdate = true
+      }
+    }
+  })
+
+  // const handleHover2 = (e) => {
+  //     e.stopPropagation(); 
+  //     handleHover(solidCapHexesArray[e.instanceId].id)
+  // }
+  // const handleUnhover2 = () => {
+  //   (e) => handleUnhover(solidCapHexesArray[e.instanceId].id)
+  // }
 
   const handleClick = (event: ThreeEvent<MouseEvent>) => {
     const hex = solidCapHexesArray[event.instanceId]
@@ -83,11 +131,17 @@ const InstanceSolidHexCap = ({
   return (
     <instancedMesh
       ref={instanceRef}
-      args={[undefined, undefined, countOfCapHexes]} //args:[geometry, material, count]
+      args={[null, null, countOfCapHexes]} //args:[geometry, material, count]
       onClick={handleClick}
+      onPointerMove={(e) => { e.stopPropagation(); handleHover(solidCapHexesArray[e.instanceId].id) }}
+      // onPointerMove={handleHover2}
+      onPointerOut={(e) => handleUnhover(solidCapHexesArray[e.instanceId].id)}
+    // onPointerOut={handleUnhover2}
     >
       <meshToonMaterial />
-      <cylinderGeometry args={[1, 1, halfLevel, 6]} />
+      <cylinderGeometry args={[1, 1, halfLevel, 6]}>
+        <instancedBufferAttribute attach="attributes-color" args={[colorArray, 3]} />
+      </cylinderGeometry>
     </instancedMesh>
   )
 }
